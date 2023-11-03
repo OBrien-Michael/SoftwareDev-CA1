7ba2d8137ab1b28bc3c80b919dd848c9488cb703
Michael O Brien <obrien.michael@outlook.ie>
2023-11-03 01:21:48 +0000

    Scorable Interface to be extended by other classes

---
+++ src/Scorable.java
@@ -0,0 +1,9 @@
+/*
+Scorable interface with calculateScore() method that must be implemented by classes that implements this one.
+*/
+
+public interface Scorable {
+
+    //Method to be implemented by classes that extend this interface to calculate the score of an exam.
+    public void calculateScore(); //Interface methods do not have a body.
+}
