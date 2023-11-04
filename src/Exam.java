cc50f37a341d7b1684ab74af87c1f9617df0c91a
Michael O Brien <obrien.michael@outlook.ie>
2023-11-04 03:19:06 +0000

    Added Constructors, getter and setter methods and changed the variables to private

--- src/Exam.java
+++ src/Exam.java
@@ -3,7 +3,52 @@
  */
 
 public abstract class Exam {
-    public int examId; //Used to uniquely identify each exam.
-    public String subject; //Used to represent the subject of the exam.
-    public int duration; //Used to store the duration of the exam in minutes.
+    private int examId; //Used to uniquely identify each exam.
+    private String subject; //Used to represent the subject of the exam.
+    private int duration; //Used to store the duration of the exam in minutes.
+
+    public int getExamId() {
+        return examId;
+    }
+
+    public void setExamId(int examId) {
+        this.examId = examId;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public Exam() {
+        this.examId = 0;
+        this.subject = "Unknown";
+        this.duration = 0;
+    }
+
+    public Exam(int examId, String subject, int duration) {
+        this.examId = examId;
+        this.subject = subject;
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Exam{" + "examId=" + examId + ", subject='" + subject + '\'' + ", duration=" + duration + '}';
+    }
+
+
+
+
 }
