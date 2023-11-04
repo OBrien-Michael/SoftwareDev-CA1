6b2422b59d0ba97c8edce72a78635ee111a87ec1
Michael O Brien <obrien.michael@outlook.ie>
2023-11-04 02:36:03 +0000

    Abstract Exam Class to hold the data of exams

---
+++ src/Exam.java
@@ -0,0 +1,9 @@
+/*
+
+ */
+
+public abstract class Exam {
+    public int examId; //Used to uniquely identify each exam.
+    public String subject; //Used to represent the subject of the exam.
+    public int duration; //Used to store the duration of the exam in minutes.
+}
