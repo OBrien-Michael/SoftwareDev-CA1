244460d9bddded6e4740604618e571c4e69e1533
Michael O Brien <obrien.michael@outlook.ie>
2023-11-06 20:26:06 +0000

    Created the Printable and Scorable interfaces to be implemented by other classes

--- src/Printable.java
+++ src/Printable.java
@@ -1,2 +1,12 @@
+/*
+Any class that implements the Printable interface must implement its methods.
+printSummaryResult and printDetailedResults take in an ExamResult ArrayList.
+ */
+import java.util.ArrayList;
+
 public interface Printable {
+
+    public void printSummaryResult(ArrayList<ExamResult> examResultArrayList);
+    public void printDetailedResults(ArrayList<ExamResult> examResultArrayList);
+
 }
--- src/Scorable.java
+++ src/Scorable.java
@@ -1,9 +1,7 @@
-/*
-Scorable interface with calculateScore() method that must be implemented by classes that implements this one.
-*/
+//Scorable interface with calculateScore() method that must be implemented by classes that implements this one.
 
 public interface Scorable {
 
-    //Method to be implemented by classes that extend this interface to calculate the score of an exam.
-    public void calculateScore(); //Interface methods do not have a body.
+    //Method to be implemented by classes that extend this interface to calculate the score of an exam and return it.
+    public int calculateScore(); //Interface methods do not have a body.
 }
