d1c6fce616e37326efbb1d25aa5bebeb4a8b140a
Michael O Brien <obrien.michael@outlook.ie>
2023-11-06 20:26:31 +0000

    Created the ExamException and StudentException classes to be thrown

--- src/ExamException.java
+++ src/ExamException.java
@@ -1,2 +1,34 @@
-public class ExamException {
+/*
+The ExamException class allows classes that extend this to throw an ExamException
+It contains a message that can be set to be thrown to the console.
+ */
+
+public class ExamException extends Exception{
+
+    //Exception message to be shown on console
+    private String examExceptionMessage;
+
+    //Set and get methods
+    public String getExamExceptionMessage() {
+        return examExceptionMessage;
+    }
+
+    public void setExamExceptionMessage(String examExceptionMessage) {
+        this.examExceptionMessage = examExceptionMessage;
+    }
+
+    //Default constructor for ExamException
+    public ExamException() {
+        this.setExamExceptionMessage("Error: Exam Exception");
+    }
+
+    //Constructor for the ExamException that takes in a message to be shown to the console
+    public ExamException(String examExceptionMessage){
+        setExamExceptionMessage(examExceptionMessage);
+    }
+
+    @Override
+    public String toString() {
+        return this.getExamExceptionMessage();
+    }
 }
--- src/StudentException.java
+++ src/StudentException.java
@@ -1,2 +1,34 @@
-public class StudentException {
+/*
+The StudentException class allows classes that extend this to throw a StudentException
+It contains a message that can be set to be thrown to the console.
+ */
+public class StudentException extends Exception{
+
+    //Exception message to be shown on console
+    private String studentExceptionMessage;
+
+    //Set and get methods for the StudentException object
+    public String getsStudentExceptionMessage() {
+        return studentExceptionMessage;
+    }
+
+    public void setStudentExceptionMessage(String studentExceptionMessage) {
+        this.studentExceptionMessage = studentExceptionMessage;
+    }
+
+    //Default constructor for StudentException
+    public StudentException() {
+        this.studentExceptionMessage = "Error: Student Exception";
+    }
+
+    //Constructor for StudentException that takes in a message to be shown to the console
+    public StudentException(String studentExceptionMessage){
+        setStudentExceptionMessage(studentExceptionMessage);
+    }
+
+    @Override
+    public String toString() {
+        return this.getsStudentExceptionMessage();
+    }
+
 }
